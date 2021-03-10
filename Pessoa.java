ee177fcbe4eeb3ca68c3e84c5dff3263ba9fa734
Nome <ramon.pereira@prof.unibh.br>
2021-03-09 21:57:14 -0300

    Adicionei getNome, setNome, calcularMatricula

--- Pessoa.java
+++ Pessoa.java
@@ -1,5 +1,17 @@
 class Pessoa {
-    private nome;
-    private matricula;
-    private sexo;
+    private String nome;
+    private int matricula;
+    private String sexo;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(nome) {
+        this.nome = nome;
+    }
+
+    public int calcularMatricula(matricula) {
+        return matricula *10;
+    }   
 }
